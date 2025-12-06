92b68d4946f6d17599caa0f894de14010a9f3bd3
puttiwutanon <leotiwutanon@gmail.com>
2025-12-06 15:50:30 +0700

    test 4

---
+++ data.js
@@ -0,0 +1,6 @@
+export const contacts = {
+    email: "leotiwutanon@gmail.com",
+    fb: "put tiwutanon",
+    ig: "pxxt.twn",
+    tel: "0950857579"
+}
--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+import { contacts } from "./data.js"
+
+const contact = document.getElementById("contact");
+const email = document.getElementById("email");
+const fb = document.getElementById("fb");
+const ig = document.getElementById("ig");
+const tel = document.getElementById("tel");
+const my_work = document.getElementById("my-work");
+const hobbies = document.getElementById("hobbies");
+
+
+my_work.addEventListener("click", ()=>{
+    alert("you came to the my work page!!!")
+})
+
+hobbies.addEventListener("click", ()=>{
+    alert("you came to the hobbies page!!!")
+})
+
+
+email.innerHTML = contacts.email
+fb.innerHTML = contacts.fb
+ig.innerHTML = contacts.ig
+tel.innerHTML = contacts.tel
+
+
+
 console.log("hello world")
